d3c6cfa30ebe8bed917aec139f77dec663b1b7b6
Markus <markus.sibbe@t-online.de>
2024-03-12 09:11:42 +0100

    fix index from slug

--- pages/art-pieces/[slug].js
+++ pages/art-pieces/[slug].js
@@ -10,8 +10,10 @@ export default function ArtPiecePage({
   const router = useRouter();
   const slug = router.query.slug;
   const artpiece = artpieces.find((artpiece) => artpiece.slug === slug);
-  const value = artpieces.indexOf((artpiece) => artpiece.slug === slug);
-  console.log("slugValue:", value);
+  //const value = artpieces.indexOf((artpiece) => artpiece.slug === slug);
+  const index = artpieces.findIndex((artpiece) => artpiece.slug === slug);
+  //console.log("slugValue:", value);
+  console.log("index:", index);
   function handleOnClick() {
     router.push("/art-pieces");
   }
@@ -19,8 +21,8 @@ export default function ArtPiecePage({
     <>
       <ArtPieceDetail artpiece={artpiece} />
       <FavoriteButton
-        favorite={favorites[value]}
-        onToggleFavorite={() => onToggleFavorite(value)}
+        favorite={favorites[index]}
+        onToggleFavorite={() => onToggleFavorite(index)}
       />
       <button onClick={handleOnClick}>Back</button>
     </>
